9f1e33414c8f9c19cb86ce70de9a6d4689559c5f
darcklito <luis_paulino5@ucne.edu.do>
2023-10-29 18:21:53 -0400

    modelo de tarea 1 añadido

---
+++ Tarea1/Shared/Prioridades.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1.Shared
+{
+    internal class Prioridades
+    {
+        [Key]
+        int PrioridadID { get; set; }
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        string? Descripción { get; set; }
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        [Range(1,int.MaxValue, ErrorMessage ="La cantidad de dias de compromiso debe ser mayor de 1")]
+        [RegularExpression(@"^\d$")]
+        int DíasCompromiso { get; set; }
+
+    }
+}
