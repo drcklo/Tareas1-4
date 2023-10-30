0c462d44fc309143f90d925e56721d074f6991e1
darcklito <luis_paulino5@ucne.edu.do>
2023-10-29 20:31:49 -0400

    Modelo de Tarea 2 implementado

---
+++ Tarea2/Shared/Clientes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2.Shared
+{
+    internal class Clientes
+    {
+        [Key]
+        int ClienteID { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        string? Nombres { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Phone(ErrorMessage = "Debe usar exclusivamente numeros y \"(,),-\" ")]
+        string? Telefono { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Phone(ErrorMessage = "Debe usar exclusivamente numeros y \"(,),-\" ")]
+        string? Celular { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "Debe ser un RNC valido")]
+        string? RNC {get;set;}
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Debe ser un correo valido")]
+        string? Email { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+
+        string? Dirección { get; set; }
+    }
+}
