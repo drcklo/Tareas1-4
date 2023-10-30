a887918fb25ec5517735adee44b9f34c753b5d9c
darcklito <luis_paulino5@ucne.edu.do>
2023-10-29 21:56:53 -0400

    Controladores de la tarea 3 creados

---
+++ Tarea3/Server/Controllers/TicketsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tarea3.Server.DAL;
+using Tarea3.Shared;
+
+namespace Tarea3.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public TicketsController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tickets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tickets>>> GetTickets()
+        {
+          if (_context.Tickets == null)
+          {
+              return NotFound();
+          }
+            return await _context.Tickets.ToListAsync();
+        }
+
+        // GET: api/Tickets/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tickets>> GetTickets(int id)
+        {
+          if (_context.Tickets == null)
+          {
+              return NotFound();
+          }
+            var tickets = await _context.Tickets.FindAsync(id);
+
+            if (tickets == null)
+            {
+                return NotFound();
+            }
+
+            return tickets;
+        }
+
+        // PUT: api/Tickets/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTickets(int id, Tickets tickets)
+        {
+            if (id != tickets.TicketsId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tickets).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tickets
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tickets>> PostTickets(Tickets tickets)
+        {
+          if (_context.Tickets == null)
+          {
+              return Problem("Entity set 'Context.Tickets'  is null.");
+          }
+            _context.Tickets.Add(tickets);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTickets", new { id = tickets.TicketsId }, tickets);
+        }
+
+        // DELETE: api/Tickets/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTickets(int id)
+        {
+            if (_context.Tickets == null)
+            {
+                return NotFound();
+            }
+            var tickets = await _context.Tickets.FindAsync(id);
+            if (tickets == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tickets.Remove(tickets);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TicketsExists(int id)
+        {
+            return (_context.Tickets?.Any(e => e.TicketsId == id)).GetValueOrDefault();
+        }
+    }
+}
