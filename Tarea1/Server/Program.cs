c8199ac505d6608858892186cb61012e5840a0b1
darcklito <luis_paulino5@ucne.edu.do>
2023-10-29 21:10:36 -0400

    Inyeccion del connectionString al program de tarea 1

--- Tarea1/Server/Program.cs
+++ Tarea1/Server/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Tarea1.Server.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Context>(op => op.UseSqlite(ConStr));
 
 var app = builder.Build();
 
