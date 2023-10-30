f1e3cf6cd7ed10bd7ae3d8609306a8e4afd7c64a
darcklito <luis_paulino5@ucne.edu.do>
2023-10-30 00:30:56 -0400

    Base de datos de la tarea 3 creada

---
+++ Tarea3/Server/Migrations/20231030042113_Primero.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarea3.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primero : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
