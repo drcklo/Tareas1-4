641a609775d5f8cbd397fbf80d8a1f1c17ad348b
darcklito <luis_paulino5@ucne.edu.do>
2023-11-01 00:12:02 -0400

    DebuggingExtensions añadidas

---
+++ Tarea4/Client/ExtensionesDeDebug.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace Tarea4.Client
+{
+    public static class ExtensionesDeDebug
+    {
+        private static JsonSerializerOptions opciones = new
+       JsonSerializerOptions
+        { WriteIndented = true };
+        public static string ToJson(this object obj)
+        => JsonSerializer.Serialize(obj, opciones);
+    }
+}
