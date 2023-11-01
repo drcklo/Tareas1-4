006dab711861879f5a4100de8e4676c33d08560a
darcklito <luis_paulino5@ucne.edu.do>
2023-11-01 00:06:20 -0400

    modelo de carrito de compra creado

---
+++ Tarea4/Shared/CarritoDeCompra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4.Shared
+{
+    public class CarritoDeCompra
+    {
+        public Clientes Clientes { get; set; } = new Clientes();
+        public List<int> Ordenes { get; set; } = new List<int>();
+        public bool Pagado { get; set; }
+        public void Agregar(int pizzaId)
+        => Ordenes.Add(pizzaId);
+        public void RemoveAt(int pos)
+        => Ordenes.RemoveAt(pos);
+
+
+    }
+}
