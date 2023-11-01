6186a2b95ba6f415fbf7dbceaa1bfc7308fce3a4
darcklito <luis_paulino5@ucne.edu.do>
2023-11-01 00:06:58 -0400

    modelo de menu creado

---
+++ Tarea4/Shared/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4.Shared
+{
+    public class Menu
+    {
+        public List<Pizzas> ListaDePizzas { get; set; }
+        = new List<Pizzas>();
+        public void AgregarPizza(Pizzas pizza)
+        => ListaDePizzas.Add(pizza);
+        public Pizzas? GetPizza(int id)
+        => ListaDePizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
