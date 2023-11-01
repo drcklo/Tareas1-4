e1c34aad510ae3cacf7ba4a5300e06119b0c48c5
darcklito <luis_paulino5@ucne.edu.do>
2023-11-01 00:06:47 -0400

    modelo de interfaz de usuario creado

---
+++ Tarea4/Shared/InterfazDeUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4.Shared
+{
+    public class InterfazDeUsuario
+    {
+        public bool MostrarCarrito { get; set; } = true;
+    }
+}
