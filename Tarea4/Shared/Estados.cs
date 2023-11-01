a2439ac9f2095602e2a97867e6695255c44e5d0e
darcklito <luis_paulino5@ucne.edu.do>
2023-11-01 00:06:32 -0400

    modelo de estados creado

---
+++ Tarea4/Shared/Estados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4.Shared
+{
+    public class Estados
+    {
+        public Menu Menu { get; } = new Menu();
+        public CarritoDeCompra Carrito { get; } = new CarritoDeCompra();
+        public InterfazDeUsuario InterfazDeUsuario { get; set; } = new InterfazDeUsuario();
+
+        public decimal PrecioTotal
+        => Carrito.Ordenes.Sum(id => Menu.GetPizza(id)!.Precio);
+    }
+}
