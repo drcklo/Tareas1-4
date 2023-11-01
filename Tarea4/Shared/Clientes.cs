632c33fd40fac64ca65dc8d5937f5aa061479b68
darcklito <luis_paulino5@ucne.edu.do>
2023-11-01 00:04:55 -0400

    modelo de clientes agredado

---
+++ Tarea4/Shared/Clientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4.Shared
+{
+    public class Clientes
+    {
+
+        public int IdCliente { get; set; }
+        [Required(ErrorMessage = "Por favor escriba un nombre")]
+        public string Nombre { get; set; } = default!;
+        [Required(ErrorMessage = "Por favor escriba una calle con su numero de casa")]
+        public string Calle { get; set; } = default!;
+        [Required(ErrorMessage = "Por favor escriba una ciudad")]
+        public string Ciudad { get; set; } = default!;
+    }
+}
