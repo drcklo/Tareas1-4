695e233cc7d08ce7987a9355bd31acdc0ada0ba1
darcklito <luis_paulino5@ucne.edu.do>
2023-11-01 00:07:34 -0400

    modelo de pizzas creado

---
+++ Tarea4/Shared/Pizzas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4.Shared
+{
+    public class Pizzas
+    {
+        public Pizzas(int id, string nombre, decimal precio,
+        NivelesDePicante nivelesDePicante)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.NivelesDePicante = nivelesDePicante;
+        }
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public NivelesDePicante NivelesDePicante { get; }
+    }
+}
